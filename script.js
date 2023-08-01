0727831b0c7a9f1ea09defb4cd29d6c32e142d2d
marcoswebmasteroficial <48739175+marcoswebmasteroficial@users.noreply.github.com>
2023-08-01 08:34:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-document.querySelector("input").addEventListener("keyup", function(e){
-let $this = e.target;
-let  str = $this.value;
-str = str.toString().toLowerCase();
-str = str.replace(/\-\-+/g, '-').replace(/^-+/, '').replace(/-+$/, '').replace(/[\s_-]+/g, '-').replace(/\s+/g, '-').replace(/[^a-z0-9 -]/g, "");
-$this.value = str;
+document.querySelector("input").addEventListener("keyup", function(e) {
+  let $this = e.target;
+  let str = $this.value;
+  str = str.toString().toLowerCase();
+  str = str.replace(/\-\-+/g, '-');
+  str = str.replace(/^-+/, ''); 
+  str = str.replace(/-+$/, ''); 
+  str = str.replace(/[\s_-]+/g, '-'); 
+  str = str.replace(/\s+/g, '-'); 
+  str = str.replace(/[^a-z0-9-]/g, ""); 
+  $this.value = str;
 });
